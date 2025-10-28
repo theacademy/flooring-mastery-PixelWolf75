f372ae0db956d2a106b19de7ee4461db70d0dadd
Eric Leon <ericleon727@gmail.com>
2025-10-28 13:24:52 +0000

    Setup the basic packages and the pom.xml to include spring which mean downgrading to java 11

--- FlooringMaster/src/main/java/com/mthree/Main.java
+++ FlooringMaster/src/main/java/com/mthree/Main.java
@@ -1,17 +1,17 @@
 package com.mthree;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Setup appContext and scan for annotations
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+        appContext.scan("com.mthree");
+        appContext.refresh();
+
+
     }
 }
