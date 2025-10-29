c40193633fc92af2cf04fd1e04c47e1dcaaaf8f3
Eric Leon <95386369+PixelWolf75@users.noreply.github.com>
2025-10-29 08:34:33 +0000

    Update IO and view

--- FlooringMaster/src/main/java/com/mthree/view/UserIOConsoleImpl.java
+++ FlooringMaster/src/main/java/com/mthree/view/UserIOConsoleImpl.java
@@ -1,9 +1,13 @@
 package com.mthree.view;
 
 import org.springframework.stereotype.Component;
+import java.util.Scanner;
 
 @Component
 public class UserIOConsoleImpl implements UserIO{
+
+    Scanner scan = new Scanner(System.in);
+
     @Override
     public void display(String message) {
         System.out.println(message);
@@ -11,21 +15,59 @@ public class UserIOConsoleImpl implements UserIO{
 
     @Override
     public String readString(String prompt) {
-        return "";
+        this.display(prompt);
+        String result = scan.nextline();
+        return result;
     }
 
     @Override
     public int readInt(String prompt) {
-        return 0;
+        do {
+            String input = this.readString(prompt);
+            try{
+                return Integer.parse(input);
+            }
+            catch(NumberFormatException e)
+            {
+                this.display("The input isn't an integer please try again");
+            }
+        } while(true);
+
+        return -1;
     }
 
     @Override
     public int readInt(String prompt, int min) {
-        return 0;
+        do {
+            int input = this.readInt(prompt);
+            if (input >= min)
+            {
+                return input;
+            }
+            else
+            {
+                this.display("The number needs to be at least " + min);
+            }
+        } while(true)
+
+        return -1;
     }
 
     @Override
     public int readInt(String prompt, int min, int max) {
-        return 0;
+        while (true)
+        {
+            int input = this.readInt(prompt, min);
+            if (input <= max)
+            {
+                return input;
+            }
+            else
+            {
+                this.display("The number needs to be at most " + max);
+            }
+        }
+
+        return -1;
     }
 }
--- FlooringMaster/src/main/java/com/mthree/view/View.java
+++ FlooringMaster/src/main/java/com/mthree/view/View.java
@@ -13,4 +13,13 @@ public class View {
     {
         io.display("This a test message which would work if spring is properly setup");
     }
+
+    public void addOrder()
+    {
+        io.display("--Enter Order Details--");
+        String customerName = io.readString("Enter the customer name:");
+        String state = io.readString("Enter State:");
+        String product = io.readString("Whatproduct are you buying?:");
+        int sqft = io.readInt("What's the area in sqft (minimum of 100sqft)", 100);
+    }
 }
