50428f8b8a46cfde8b7582cda49bee030ef9387f
Eric Leon <ericleon727@gmail.com>
2025-10-30 11:05:03 +0000

    test files

--- FlooringMaster/src/test/java/com/mthree/dao/TestProductDao.java
+++ FlooringMaster/src/test/java/com/mthree/dao/TestProductDao.java
@@ -1,4 +1,25 @@
 package com.mthree.dao;
 
+import com.mthree.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class TestProductDao {
+    ProductDao dao;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        dao = new ProductDaoFileImpl();
+    }
+
+    @Test
+    void testGetAllProducts() {
+        List<Product> products = dao.getAllProducts();
+        assertNotEquals(0, products.size());
+    }
 }
--- FlooringMaster/src/test/java/com/mthree/dao/TestTaxDao.java
+++ FlooringMaster/src/test/java/com/mthree/dao/TestTaxDao.java
@@ -1,4 +1,25 @@
 package com.mthree.dao;
 
+import com.mthree.model.Product;
+import com.mthree.model.Tax;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class TestTaxDao {
+    TaxDao dao;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        dao = new TaxDaoFileImpl();
+    }
+
+    @Test
+    void testGetAllProducts() {
+        List<Tax> taxes = dao.getAllTaxes();
+        assertNotEquals(0, taxes.size());
+    }
 }
